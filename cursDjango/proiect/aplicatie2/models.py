6805dee3ef9181203c27b7f18d9a769d07b271b6
Alexandra Bratu <alexandra.bratu@nxdata.ro>
2021-12-02 19:40:55 +0200

    django curs 4

--- cursDjango/proiect/aplicatie2/models.py
+++ cursDjango/proiect/aplicatie2/models.py
@@ -1,6 +1,5 @@
 from django.db import models
-
-# Create your models here.
+from aplicatie1.models import Location
 
 
 class Logs(models.Model):
@@ -21,3 +20,16 @@ class Pontaj(models.Model):
     user = models.ForeignKey('auth.User', on_delete=models.CASCADE)
     start_date = models.DateTimeField()
     end_date = models.DateTimeField(null=True)
+
+
+class Companies(models.Model):
+    company_choices = (('SRL', 'S.R.L.'),
+                       ('SA', 'S.A.'))
+
+    name = models.CharField(max_length=100)
+    website = models.CharField(max_length=50)
+    company_type = models.CharField(max_length=5, choices=company_choices)
+    location = models.ForeignKey(Location, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f"{self.company_type} {self.name}"
--- cursDjango/proiect/aplicatie2/urls.py
+++ cursDjango/proiect/aplicatie2/urls.py
@@ -6,4 +6,7 @@ app_name = 'aplicatie2'
 urlpatterns = [
     path('new_timesheet', views.newPontaj, name='pontaj'),
     path('stop_timesheet', views.stopTimesheet, name='oprire_pontaj'),
+    path('add/', views.CreateCompaniesView.as_view(), name='adaugare'),
+    path('edit/<int:pk>/', views.UpdateCompaniesView.as_view(), name='modificare'),
+    path('index/', views.ListCompaniesView.as_view(), name='lista'),
 ]
